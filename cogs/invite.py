56bb76399d67c9038564d8671b6de01ab3da9870
MR-A <52797899+MR-A0101@users.noreply.github.com>
2021-07-15 16:15:52 +0000

    py-crypt_files

--- cogs/invite.py
+++ cogs/invite.py
@@ -1,6 +1,5 @@
 import discord 
 from discord.ext import commands
-from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType
 
 class InviteCog(commands.Cog):
   def __init__(self, client):
